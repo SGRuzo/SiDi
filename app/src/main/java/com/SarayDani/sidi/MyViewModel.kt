ba800c80da23178ca89ae5e7f430a8d8364fce01
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-11-11 12:44:52 +0100

    Implementar MyViewModel con estado inicial y función para generar números aleatorios

--- app/src/main/java/com/SarayDani/sidi/MyViewModel.kt
+++ app/src/main/java/com/SarayDani/sidi/MyViewModel.kt
@@ -1,4 +1,25 @@
 package com.SarayDani.sidi
 
-class MyViewModel{
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MyViewModel(): ViewModel() {
+
+    private val TAG_LOG = "miDebug"
+    val estadoActual: MutableStateFlow<Estados> = MutableStateFlow(Estados.Inicio)
+
+    var _numbers = mutableListOf<Int>()
+
+    init {
+        // estado inicial
+        Log.d(TAG_LOG, "Inicializamos ViewModel - Estado: ${estadoActual.value}")
+    }
+
+    fun crearRandom() {
+        estadoActual.value = Estados.GenerarSecuencia
+        _numbers.add((0..3).random())
+        Log.d(TAG_LOG, "creamos random ${_numbers} - Estado: ${estadoActual.value}")
+
+    }
 }
